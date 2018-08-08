56b9863e8a6daf221956a640bc92a5e8dfeb4d95
mcurrr <technosigal@gmail.com>
2018-08-08 23:41:30 +0300

    intial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Timer
+{
+    class Hello
+    {
+        static void Main()
+        {
+            var stopwatch = new Stopwatch();
+            Console.WriteLine("Press any key to start timer");
+            Console.ReadKey();
+            stopwatch.Start();
+            stopwatch.Start();
+
+            Console.WriteLine("Press any key to stop timer");
+            Console.ReadKey();
+            stopwatch.Stop();
+
+            Console.WriteLine("Press any key to start timer again");
+            Console.ReadKey();
+            stopwatch.Start();
+
+            Console.WriteLine("Press any key to stop timer");
+            Console.ReadKey();
+            stopwatch.Stop();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Timer{
+    public class Stopwatch
+    {
+        public TimeSpan Duration { get; set; }
+        public DateTime StartTime { get; set; }
+        private bool _inProgress = false;
+
+        public void Start()
+        {
+            if (this._inProgress) return;
+
+            this._inProgress = true;
+            this.StartTime = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            if (!this._inProgress) return;
+
+            TimeSpan currentDiff = DateTime.Now - this.StartTime;
+            this.Duration += currentDiff;
+            this._inProgress = false;
+
+            this.ShowDuration(this.Duration);
+        }
+
+        private void ShowDuration(TimeSpan duration)
+        {
+            Console.WriteLine("Current duration is ${0}", duration);
+        }
+
+        public void WhatTimeIsIt()
+        {
+            this.ShowDuration(this.Duration);
+        }
+    }
+}
